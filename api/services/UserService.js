fb5e7c20b8e4c678320774acda46b63f95b914ac
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2016-10-09 11:46:31 +0530

    add user service method to get an array of users when an id list is provided

--- api/services/UserService.js
+++ api/services/UserService.js
@@ -29,6 +29,47 @@ var UserService = {
 
   },
 
+  /**
+   * get a list of users
+   * with contact details
+   */
+  getUserList: function (options, getUsersCallback){
+    var userArray = [];
+
+    // variable to count items processed inorder to call callback when all items are processed
+    var items_processed = 0;  
+
+    var id_list = options.user_id_list;
+
+    for(id of id_list){
+      // db query
+      User.find(id).exec(function(error, users){
+        items_processed++;
+        
+        if (error) {
+          // handle error here- e.g. `res.serverError(err);`
+          return;
+
+        }else if ( users.length > 0){
+          var user = users[0];
+          // temp user object
+          var tempUser = {};
+          tempUser.name = user.firstName + ' ' + user.lastName;
+          tempUser.id = user.id;
+          tempUser.email = user.email;
+          tempUser.contactNumber = user.contactNumber;
+          tempUser.designation = user.designation;
+          userArray.push(tempUser);
+
+          // call callback when all items are processed  
+          if(items_processed == id_list.length){
+            getUsersCallback(null, userArray);
+          }  
+        }
+      });        
+    }    
+  },
+
 };
 
 module.exports = UserService;
